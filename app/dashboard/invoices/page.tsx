32403c2097619a2a38c9f2b4c5fdfc846c2f04f6
Nur Syamsu <nursyamsu.official@gmail.com>
2024-09-24 00:29:06 +0700

    update until lesson 9

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,22 @@
 export default function() {
-    return <p>Invoice pages</p>
+    return (
+    <>
+        <p>Invoice pages</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eveniet distinctio molestias quibusdam repellat provident velit consequuntur voluptatem asperiores fugiat. Ab distinctio sit quibusdam corporis voluptatum minus, obcaecati voluptatem numquam.</p>
+    </>
+    ) 
 }
